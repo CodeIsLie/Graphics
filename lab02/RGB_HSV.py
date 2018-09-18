c61d6c1559bb6d36d2fbc3a14cab52bf15bc3d24
CodeIsLie <ilya1757@gmail.com>
2018-09-18 21:12:45 +0300

    HSV2RGB gunction complete

--- lab02/RGB_HSV.py
+++ lab02/RGB_HSV.py
@@ -9,62 +9,91 @@ from tkinter import filedialog
 from tkinter import *
 from PIL import Image
 from PIL import ImageTk
+from numpy import dstack
 
 # import tkFileDialog
 
-
 def RGB2HSV(img):
-    return img
+    float_img = cv2.normalize(img.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
+    R = float_img[:, :, 0].tolist()
+    G = float_img[:, :, 1].tolist()
+    B = float_img[:, :, 2].tolist()
+
+    MAX = [max(r, g, b) for r, g, b in zip(R, G, B)]
+    MIN = [min(r, g, b) for r, g, b in zip(R, G, B)]
+
+    H = [[0 if max1 == min1 else
+         60 * (g - b) // (max1 - min1) + 0 if max == r and g >= b else
+         60 * (g - b) // (max1 - min1) + 360 if max == r else
+         60 * (b - r) // (max1 - min1) + 120 if max == g else
+         60 * (r - g) // (max1 - min1) + 240
+         for max1, min1, r, g, b in zip(maxes, mines, rs, gs, bs)]
+          for maxes, mines, rs, gs, bs in zip(MAX, MIN, R, G, B)]
+    S = [[0 if max1 == 0 else 1 - min1/max1 for min1, max1 in zip(mins, maxes)] for mins, maxes in zip(MIN, MAX)]
+    V = MAX
+
+    HSV_img = dstack((H, S, V))
+    return HSV_img
 
 def HSV2RGB(img):
     return img
 
-def imgWork():
-    filename = "img1.png"
-    img = imread(filename)
-
-    # change scrollbars -> change channel by modify original channel, assembly img, HSV2RGB
-    # redraw imagebox
-    # imgcopy
-    # if click on button, save img to file
-
-    hsv_img = RGB2HSV(img)
-
+def getImg(path):
+    img = imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(img)
+    image = ImageTk.PhotoImage(image)
+    return image
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
 
+        global panelA
+        # panelA = None
+        self.createGui()
+
+    def createGui(self):
         self.grid()
         self.master.title("Channels changes")
-
-        filename = "img1.png"
-        img = imread(filename)
-
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(img)
-        image = ImageTk.PhotoImage(image)
-
-        #global panelA
-        # panelA = None
-        panelA = Label(image=image)
-        panelA.image = image
-        panelA.grid(row=0, column=0, rowspan=5, columnspan=2, sticky=W + E + N + S)
 
         for r in range(6):
             self.master.rowconfigure(r, weight=1)
         for c in range(5):
             self.master.columnconfigure(c, weight=1)
 
+        filename = "img1.png"
+        img = imread(filename)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        self.RGB_img = img
+        self.HSV_img = RGB2HSV(img)
+        image = RGB2HSV(img)
+
+        image = Image.fromarray(image)
+        image = ImageTk.PhotoImage(image)
+
+        panelA = Label(image=image)
+        panelA.image = image
+        panelA.grid(row=0, column=0, rowspan=5, columnspan=2, sticky=W + E + N + S)
+
+        self.createScales()
+        self.createButtons()
+
+    def createButtons(self):
         def choose_img():
             path = filedialog.askopenfilename()
             if len(path) < 1:
                 return
+
             img = imread(path)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            self.RGB_img = img
+            self.HSV_img = RGB2HSV(img)
+
             image = Image.fromarray(img)
             image = ImageTk.PhotoImage(image)
-
             panelA.configure(image=image)
             panelA.image = image
 
@@ -78,6 +107,7 @@ class Application(tk.Frame):
         self.save_button["command"] = root.destroy
         self.save_button.grid(row=5, column=3, rowspan=1, columnspan=4, sticky=W + E)
 
+    def createScales(self):
         def changeHue(hue):
             pass
 
@@ -89,7 +119,7 @@ class Application(tk.Frame):
 
         varH = DoubleVar(value=120)
         scaleH = Scale(root, variable=varH, from_=0, to=255, command=changeHue)
-        scaleH.grid(row = 0, column = 3, rowspan=5, columnspan = 1, sticky = N+S)
+        scaleH.grid(row=0, column=3, rowspan=5, columnspan=1, sticky=N + S)
 
         varS = DoubleVar(value=120)
         scaleS = Scale(root, variable=varS, from_=0, to=255, command=changeSaturation)
@@ -98,8 +128,6 @@ class Application(tk.Frame):
         varV = DoubleVar(value=120)
         scaleV = Scale(root, variable=varV, from_=0, to=255, command=changeValue)
         scaleV.grid(row=0, column=5, rowspan=5, columnspan=4, sticky=N + S)
-
-
 
 root = tk.Tk()
 app = Application(master=root)
