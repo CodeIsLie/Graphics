cc6c0f171f4ec0337ace4512f86980dadf295230
Ilya Yakubovsky <ilya1757@gmail.com>
2019-03-22 17:58:08 +0300

    display is added

---
+++ natali_2lab/Gui.py
@@ -0,0 +1,233 @@
+from Object3D import *
+from tkinter import *
+from PIL import Image, ImageTk, ImageDraw
+
+class WorkArea:
+    DEFAULT_WIDTH = 600
+    DEFAULT_HEIGHT = 600
+    DEFAULT_COLOR = 'black'
+
+    def __init__(self):
+        self.figure = Display()
+        print(self.figure.point_list)
+        print(self.figure.edges)
+        self.figure.scale(6, 6, 6)
+        self.figure.rotate_y_axis_center(np.pi)
+        self.figure.shift(100, 100, 130)
+        self.projection_type = Projection.ORTHO_XOY
+
+        self.iso_mode = "iso_1"
+        self.root = Tk()
+        self.root.title("3DPRO")
+        self.root.resizable(False, False)
+
+        self.canvas = Canvas(self.root, bg='white', width=self.DEFAULT_WIDTH, height=self.DEFAULT_WIDTH)
+        self.canvas.grid(row=1, columnspan=10)
+        self.image = Image.new('RGB', (self.DEFAULT_WIDTH, self.DEFAULT_HEIGHT), 'white')
+        self.draw = ImageDraw.Draw(self.image)
+
+        Label(self.root, text="Тип проекции:             ").grid(row=2, column=1)
+        self.projection_var = StringVar(self.root)
+        self.projection_var.set("orthographic_xoy")  # default value
+        possible_projections = ["orthographic_xoy",
+                                "orthographic_xoz",
+                                "orthographic_yoz",
+                                "isometric",
+                                "dimetric",
+                                "perspective_one",
+                                "perspective_two",
+                                "perspective_three"]
+        self.choose_projection = OptionMenu(self.root, self.projection_var, *possible_projections)
+        self.choose_projection.grid(row=3, column=1)
+
+        self.projection_button = Button(self.root, text='Apply', command=self.select_projection)
+        self.projection_button.grid(row=4, column=1)
+
+        Label(self.root, text="x: ").grid(row=2, column=2)
+        Label(self.root, text="y: ").grid(row=3, column=2)
+        Label(self.root, text="z: ").grid(row=4, column=2)
+
+        self.x_input_box = Entry(self.root)
+        self.y_input_box = Entry(self.root)
+        self.z_input_box = Entry(self.root)
+        self.x_input_box.insert(0, "100")
+        self.y_input_box.insert(0, "100")
+        self.z_input_box.insert(0, "100")
+        self.x_input_box.grid(row=2, column=3)
+        self.y_input_box.grid(row=3, column=3)
+        self.z_input_box.grid(row=4, column=3)
+        self.shift_button = Button(self.root, text='Shift', command=self.shift).grid(row=5, column=3)
+
+        Label(self.root, text="angle: ").grid(row=2, column=6)
+        self.angle_input_box = Entry(self.root)
+        self.angle_input_box.insert(0, "45")
+        self.angle_input_box.grid(row=3, column=6)
+
+        self.x_rotate_button = Button(self.root, text='rotate about x', command=self.x_rotate).grid(row=4, column=6)
+        self.y_rotate_button = Button(self.root, text='rotate about y', command=self.y_rotate).grid(row=5, column=6)
+        self.z_rotate_button = Button(self.root, text='rotate about z', command=self.z_rotate).grid(row=6, column=6)
+
+        Label(self.root, text="kx: ").grid(row=2, column=4)
+        Label(self.root, text="ky: ").grid(row=3, column=4)
+        Label(self.root, text="kz: ").grid(row=4, column=4)
+
+        self.kx_input_box = Entry(self.root)
+        self.ky_input_box = Entry(self.root)
+        self.kz_input_box = Entry(self.root)
+        self.kx_input_box.insert(0, "1")
+        self.ky_input_box.insert(0, "1")
+        self.kz_input_box.insert(0, "1")
+        self.kx_input_box.grid(row=2, column=5)
+        self.ky_input_box.grid(row=3, column=5)
+        self.kz_input_box.grid(row=4, column=5)
+        self.scale_button = Button(self.root, text='Scale', command=self.scale).grid(row=5, column=5)
+
+        self.eraser_button = Button(self.root, text='Clear', command=self.erase)
+        self.eraser_button.grid(row=3, column=8)
+
+        self.redraw_button = Button(self.root, text='Redraw', command=self.redraw_all)
+        self.redraw_button.grid(row=2, column=8)
+
+        self.redraw_button = Button(self.root, text='Next', command=self.next_perspective)
+        self.redraw_button.grid(row=4, column=8)
+
+        self.redraw_all()
+
+        self.root.mainloop()
+
+    def project(self):
+        projection_type = self.projection_type
+        figure = self.figure
+        # Orthographic projections
+        if projection_type == Projection.ORTHO_XOY:
+            return figure.orthographic_XOY().take_xy_coords()
+        elif projection_type == Projection.ORTHO_XOZ:
+            return figure.orthographic_XOZ().take_xz_coords()
+        elif projection_type == Projection.ORTHO_YOZ:
+            return figure.orthographic_YOZ().take_yz_coords()
+        # Axonometric projections
+        elif projection_type == Projection.ISOMETRIC:
+            return figure.isometric().take_xy_coords()
+        elif projection_type == Projection.DIMETRIC:
+            return figure.dimetric().take_xy_coords()
+        # Perspective projections
+        elif projection_type == Projection.PERSPECTIVE_1:
+            return figure.perspective_one_point().take_xy_coords()
+        elif projection_type == Projection.PERSPECTIVE_2:
+            return figure.perspective_two_point().take_xy_coords()
+        elif projection_type == Projection.PERSPECTIVE_3:
+            return figure.perspective_three_point().take_xy_coords()
+
+    def select_projection(self):
+        projection_dict = {
+            "orthographic_xoy": Projection.ORTHO_XOY,
+            "orthographic_xoz": Projection.ORTHO_XOZ,
+            "orthographic_yoz": Projection.ORTHO_YOZ,
+            "isometric": Projection.ISOMETRIC,
+            "dimetric": Projection.DIMETRIC,
+            "perspective_one": Projection.PERSPECTIVE_1,
+            "perspective_two": Projection.PERSPECTIVE_2,
+            "perspective_three": Projection.PERSPECTIVE_3,
+        }
+        self.projection_type = projection_dict[self.projection_var.get()]
+        self.redraw_all()
+
+    def shift(self):
+        figure = self.figure
+        dx = float(self.x_input_box.get())
+        dy = float(self.y_input_box.get())
+        dz = float(self.z_input_box.get())
+        figure.shift(dx, dy, dz)
+        print("success translate dx={} dy={} dz={}".format(dx, dy, dz))
+        self.redraw_all()
+
+    def center_transform(func):
+        def magic(self):
+            mid_x, mid_y, mid_z = self.figure.center_point
+            self.figure.shift(-mid_x, -mid_y, -mid_z)
+            func(self)
+            self.figure.shift(mid_x, mid_y, mid_z)
+            self.redraw_all()
+        return magic
+
+    @center_transform
+    def scale(self):
+        figure = self.figure
+        mx = float(self.kx_input_box.get())
+        my = float(self.ky_input_box.get())
+        mz = float(self.kz_input_box.get())
+        figure.scale(mx, my, mz)
+        print("success scale mx={} my={} mz={}".format(mx, my, mz))
+
+    @center_transform
+    def x_rotate(self):
+        angle = float(self.angle_input_box.get()) * np.pi/ 180
+        self.figure.rotate_x_axis(angle)
+
+    @center_transform
+    def y_rotate(self):
+        angle = float(self.angle_input_box.get()) * np.pi/ 180
+        self.figure.rotate_y_axis(angle)
+
+    @center_transform
+    def z_rotate(self):
+        angle = float(self.angle_input_box.get()) * np.pi/ 180
+        self.figure.rotate_z_axis(angle)
+
+    def next_perspective(self):
+        if self.projection_var.get() == "orthographic_xoy":
+            self.projection_var.set("orthographic_xoz")
+        elif self.projection_var.get() == "orthographic_xoz":
+            self.figure.rotate_x_axis_center(np.pi / 180)
+            self.projection_var.set("orthographic_yoz")
+        elif self.projection_var.get() == "orthographic_yoz":
+            self.figure.rotate_x_axis_center(90 * np.pi / 180)
+            self.figure.rotate_z_axis_center(180 * np.pi / 180)
+            self.projection_var.set("isometric")
+        elif self.projection_var.get() == "isometric" and self.iso_mode == "iso_1":
+            self.figure.scale_center(0.5, 1, 1)
+            self.iso_mode = "iso_2"
+        elif self.projection_var.get() == "isometric" and self.iso_mode == "iso_2":
+            self.figure.scale_center(2, 0.5, 1)
+            self.iso_mode = "iso_3"
+        elif self.projection_var.get() == "isometric" and self.iso_mode == "iso_3":
+            self.figure.scale_center(1, 2, 1)
+            self.projection_var.set("dimetric")
+        elif self.projection_var.get() == "dimetric":
+            self.figure.scale_center(1.4, 1.4, 1.4)
+
+            self.figure.rotate_y_axis(70 * np.pi/180)
+            self.projection_var.set("perspective_one")
+        elif self.projection_var.get() == "perspective_one":
+            self.figure = Display()
+            self.figure.rotate_x_axis_center(115 * np.pi/180)
+            self.figure.scale_center(1.2, 1.2, 1.2)
+            self.figure.shift(200, 200, 20)
+            self.projection_var.set("perspective_two")
+        elif self.projection_var.get() == "perspective_two":
+            # self.figure = Chair()
+            self.figure.scale_center(1.5, 1.5, 1.5)
+            self.figure.rotate_x_axis_center(15 * np.pi/180)
+            self.projection_var.set("perspective_three")
+
+        self.select_projection()
+
+    def redraw_all(self):
+        self.erase()
+        # self.figure.draw(self.draw, self.projection)
+        figure_projection = self.project()
+        for point_ind1, point_ind2 in figure_projection.edges:
+            x1, y1 = figure_projection.point_list[point_ind1]
+            x2, y2 = figure_projection.point_list[point_ind2]
+            self.draw.line([x1, y1, x2, y2], width=1, fill=WorkArea.DEFAULT_COLOR)
+
+        self.canvas.image = ImageTk.PhotoImage(self.image)
+        self.canvas.create_image(0, 0, image=self.canvas.image, anchor='nw')
+
+    def erase(self):
+        self.canvas.delete("all")
+        self.image = Image.new('RGB', (self.DEFAULT_WIDTH, self.DEFAULT_WIDTH), 'white')
+        self.draw = ImageDraw.Draw(self.image)
+
+
+gui = WorkArea()
---
+++ natali_2lab/Object3D.py
@@ -0,0 +1,337 @@
+import numpy as np
+import copy
+from enum import Enum
+
+class Projection(Enum):
+    ORTHO_XOY = 1
+    ORTHO_XOZ = 2
+    ORTHO_YOZ = 3
+
+    ISOMETRIC = 4
+    DIMETRIC = 5
+
+    PERSPECTIVE_1 = 6
+    PERSPECTIVE_2 = 7
+    PERSPECTIVE_3 = 8
+
+
+def point_transfrom(point, matrix):
+    row_transformation = None
+    point = np.append(point, [1])
+    row_transformation = np.dot(point, matrix)
+    if abs(row_transformation[3]) < 1e-10:
+        x = 5
+    homogenious_coords = row_transformation / row_transformation[3]
+
+    return homogenious_coords[:3]
+
+def make_clone(figure):
+    points_copy = copy.deepcopy(figure.point_list)
+    return Figure(points_copy, figure.edges)
+
+def get_shift_matrix(dx, dy, dz):
+    shift_matrix = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [dx, dy, dz, 1]
+    ])
+    return shift_matrix
+
+def get_x_rot_matrix(angle):
+    cos_a = np.cos(angle)
+    sin_a = np.sin(angle)
+    rotation_matrix = np.array([
+        [1,     0,      0, 0],
+        [0, cos_a,  sin_a, 0],
+        [0, -sin_a, cos_a, 0],
+        [0,      0,     0, 1]
+    ])
+    return rotation_matrix
+
+def get_y_rot_matrix(angle):
+    cos_a = np.cos(angle)
+    sin_a = np.sin(angle)
+    rotation_matrix = np.array([
+        [cos_a, 0, -sin_a, 0],
+        [0,     1,      0, 0],
+        [sin_a, 0,  cos_a, 0],
+        [0,     0,      0, 1]
+    ])
+    return rotation_matrix
+
+def get_z_rot_matrix(angle):
+    cos_a = np.cos(angle)
+    sin_a = np.sin(angle)
+    rotation_matrix = np.array([
+        [cos_a,  sin_a, 0, 0],
+        [-sin_a, cos_a, 0, 0],
+        [0,          0, 1, 0],
+        [0,          0, 0, 1]
+    ])
+    return rotation_matrix
+
+
+class Figure:
+    def __init__(self, point_list, edges=None):
+        self.point_list = np.array(point_list)
+        self.center_point = None
+        self.calc_center()
+        self.edges = edges
+
+    def calc_center(self):
+        mid_x = (max(self.point_list[:, 0]) + min(self.point_list[:, 0])) / 2
+        mid_y = (max(self.point_list[:, 1]) + min(self.point_list[:, 1])) / 2
+        mid_z = (max(self.point_list[:, 2]) + min(self.point_list[:, 2])) / 2
+        self.center_point = np.array([mid_x, mid_y, mid_z])
+
+    def take_xy_coords(self):
+        self.point_list = [np.take(x, [0, 1]) for x in self.point_list]
+        return self
+
+    def take_xz_coords(self):
+        self.point_list = [np.take(x, [0, 2]) for x in self.point_list]
+        return self
+
+    def take_yz_coords(self):
+        self.point_list = [np.take(x, [1, 2]) for x in self.point_list]
+        return self
+
+    def transform(self, matrix):
+        self.center_point = point_transfrom(self.center_point, matrix)
+        self.point_list = [point_transfrom(p, matrix) for p in self.point_list]
+
+    def scale(self, kx, ky, kz):
+        scale_matrix = np.array([
+            [kx, 0, 0, 0],
+            [0, ky, 0, 0],
+            [0, 0, kz, 0],
+            [0, 0, 0,  1]
+        ])
+        self.transform(scale_matrix)
+
+    def scale_center(self, kx, ky, kz):
+        mid_x, mid_y, mid_z = self.center_point
+        self.shift(-mid_x, -mid_y, -mid_z)
+        self.scale(kx, ky, kz)
+        self.shift(mid_x, mid_y, mid_z)
+
+    def shift(self, dx, dy, dz):
+        shift_matrix = get_shift_matrix(dx, dy, dz)
+        self.transform(shift_matrix)
+
+    def rotate_x_axis(self, angle):
+        rotation_matrix = get_x_rot_matrix(angle)
+        self.transform(rotation_matrix)
+
+    def rotate_y_axis(self, angle):
+        rotation_matrix = get_y_rot_matrix(angle)
+        self.transform(rotation_matrix)
+
+    def rotate_z_axis(self, angle):
+        rotation_matrix = get_z_rot_matrix(angle)
+        self.transform(rotation_matrix)
+
+    def rotate_x_axis_center(self, angle):
+        mid_x, mid_y, mid_z = self.center_point
+        self.shift(-mid_x, -mid_y, -mid_z)
+        rotation_matrix = get_x_rot_matrix(angle)
+        self.transform(rotation_matrix)
+        self.shift(mid_x, mid_y, mid_z)
+
+    def rotate_y_axis_center(self, angle):
+        mid_x, mid_y, mid_z = self.center_point
+        self.shift(-mid_x, -mid_y, -mid_z)
+        rotation_matrix = get_y_rot_matrix(angle)
+        self.transform(rotation_matrix)
+        self.shift(mid_x, mid_y, mid_z)
+
+    def rotate_z_axis_center(self, angle):
+        mid_x, mid_y, mid_z = self.center_point
+        self.shift(-mid_x, -mid_y, -mid_z)
+        rotation_matrix = get_z_rot_matrix(angle)
+        self.transform(rotation_matrix)
+        self.shift(mid_x, mid_y, mid_z)
+
+    def get_projection(self, matrix):
+        clone = make_clone(self)
+        clone.transform(matrix)
+        return clone
+
+    def orthographic_XOY(self):
+        ortho_matrix = np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 1]
+        ])
+        return self.get_projection(ortho_matrix)
+
+    def orthographic_XOZ(self):
+        ortho_matrix = np.array([
+            [1, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ])
+        return self.get_projection(ortho_matrix)
+
+    def orthographic_YOZ(self):
+        ortho_matrix = np.array([
+            [0, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ])
+        return self.get_projection(ortho_matrix)
+
+    def isometric(self):
+        isometric_mat = np.dot(get_y_rot_matrix(np.pi*45/180),
+                               get_x_rot_matrix(np.pi*35.264/180))
+        dx, dy, dz = self.center_point
+        matrix = np.dot(np.dot(get_shift_matrix(-dx, -dy, -dz), isometric_mat), get_shift_matrix(dx, dy, dz))
+        projection = self.get_projection(matrix)
+        return projection
+
+    def dimetric(self):
+        dimetric_mat = np.dot(get_y_rot_matrix(np.pi*45/180),
+                              get_x_rot_matrix(np.pi*26.565/180))
+        dx, dy, dz = self.center_point
+        matrix = np.dot(np.dot(get_shift_matrix(-dx, -dy, -dz), dimetric_mat), get_shift_matrix(dx, dy, dz))
+        projection = self.get_projection(matrix)
+        return projection
+
+    def perspective_one_point(self, d=200):
+        perspective_mat = np.array([
+            [1, 0, 0,   0],
+            [0, 1, 0,   0],
+            [0, 0, 1, 1/d],
+            [0, 0, 0,   1]
+        ])
+        return self.get_projection(perspective_mat)
+
+    def perspective_two_point(self, dy=200, dz=200):
+        perspective_mat = np.array([
+            [1, 0, 0,     0],
+            [0, 1, 0, 1/dy],
+            [0, 0, 1, 1/dz],
+            [0, 0, 0,     0]
+        ])
+        return self.get_projection(perspective_mat)
+
+    def perspective_three_point(self, dx=200, dy=200, dz=200):
+        perspective_mat = np.array([
+            [1, 0, 0, 1/dx],
+            [0, 1, 0, 1/dy],
+            [0, 0, 1, 1/dz],
+            [0, 0, 0,     0]
+        ])
+        return self.get_projection(perspective_mat)
+
+
+class Cube(Figure):
+    def __init__(self):
+        point_list = [
+            [0, 0, 0],
+            [0, 0, 1],
+            [0, 1, 0],
+            [1, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 0],
+            [1, 1, 1]
+        ]
+        edges = [
+            (0, 1),
+            (0, 2),
+            (0, 3),
+            (1, 4),
+            (1, 5),
+            (2, 4),
+            (2, 6),
+            (3, 5),
+            (3, 6),
+            (7, 4),
+            (7, 5),
+            (7, 6)
+        ]
+        Figure.__init__(self, point_list, edges)
+
+
+class Cylinder(Figure):
+    """
+    единичная длина, единичный радиус
+    """
+    def __init__(self, segments_count):
+        angle_step = (np.pi/180) * 360 / segments_count
+        first_point = np.array([1, 0, 0])
+        first_behind_point = np.array([1, 0, 1])
+
+        points = [first_point, first_behind_point]
+        edges = [(0, 1), (0, 2), (1, 3)]
+        current_angle = 0
+        index = 0
+        all_cnt_points = 2*segments_count
+        for _ in range(segments_count-1):
+            current_angle += angle_step
+            index += 2
+            new_point = np.array([np.cos(current_angle), np.sin(current_angle), 0])
+            behind_point = new_point + np.array([0, 0, 1])
+            points.append(new_point)
+            points.append(behind_point)
+
+            edges += [(index, index+1), (index, (index+2) % all_cnt_points),
+                      (index+1, (index+3) % all_cnt_points)]
+
+        Figure.__init__(self, points, edges)
+
+
+class Display(Figure):
+    def __init__(self):
+        cnt_segments = 12
+        high_cylinder = Cylinder(cnt_segments)
+        high_cylinder.scale_center(4.5, 4.5, 5)
+        high_cylinder.shift(21, 13, 7.5)
+
+        low_cylinder = Cylinder(cnt_segments)
+        low_cylinder.scale_center(13, 13, 1)
+        low_cylinder.shift(21, 13, 0.5)
+
+        offset = cnt_segments*2
+        leg = Figure(low_cylinder.point_list + high_cylinder.point_list,
+                     low_cylinder.edges +
+                     [(x + offset, y+offset) for x, y in high_cylinder.edges] +
+                     [(2*i+offset, 2*i+1) for i in range(cnt_segments)]
+                     )
+
+        front_cube = Cube()
+        front_cube.scale_center(42, 18, 40)
+        front_cube.shift(21, 8.5, 30)
+
+        back_cube = Cube()
+        back_cube.scale_center(32, 11, 26)
+        back_cube.shift(21, 39.5, 30)
+
+        offset = 8
+        display = Figure(front_cube.point_list + back_cube.point_list,
+                         front_cube.edges +
+                         [(x + offset, y + offset) for x, y in back_cube.edges] +
+                         [(x + offset, y) for x, y in zip([0, 1, 3, 5], [2, 4, 6, 7])]
+                         )
+
+        frame = Figure([np.array([0, 0, 0]),
+                        np.array([0, 0, 1]),
+                        np.array([1, 0, 1]),
+                        np.array([1, 0, 0])],
+                       [(0, 1), (1, 2), (2, 3), (3, 0)])
+        frame.scale(38, 1, 36)
+        frame.shift(2, 0, 12)
+
+        offset = 0
+        all_points = []
+        all_edges = []
+        for obj, step in zip([leg, display, frame], [cnt_segments*4, 16, 4]):
+            all_points += list(obj.point_list)
+            all_edges += [(x+offset, y+offset) for x, y in obj.edges]
+            offset += step
+        Figure.__init__(self, all_points, all_edges)
